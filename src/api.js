61c8d2ea30b6c4018b1cef51e99eca1d55fb9af5
Darren Kim <dkim555@yahoo.com>
2023-04-28 14:42:51 -0700

    add css

--- src/api.js
+++ src/api.js
@@ -1,12 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const searchImages = () => {
-    axios.get('http://api.unsplash.com/search/photos', {
+const searchImages = async (term) => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
-        Authorization: 'Client-ID DB4TkZktmlRd0VNELSdhHorGaqsua8T-HYZuTI26KeM'
+      Authorization: 'Client-ID DB4TkZktmlRd0VNELSdhHorGaqsua8T-HYZuTI26KeM',
     },
     params: {
-        query: 'cars'
+      query: term,
     },
-    });
-}
+  });
+
+
+  return response.data.results;
+};
+
+export default searchImages;
